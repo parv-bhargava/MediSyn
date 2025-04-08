6d470aa9677659af06eb1c43e7bef7e87dad995a
Sajan Kumar Kar <sajan.kumar.kar@gmail.com>
2025-04-08 15:34:32 -0400

    Bar plots added for LLM prepared data

--- code/src/eval/analysis.py
+++ code/src/eval/analysis.py
@@ -10,6 +10,9 @@ print(os.getcwd())
 manual_claude_df=pd.read_excel("outputs_manual_claude/evaluation_results_['anthropic.claude-3-5-sonnet-20240620-v1:0'].xlsx")
 manual_gpt_df = pd.read_excel("outputs_manual_gpt4o/evaluation_results_['gpt-4o'].xlsx")
 manual_llama_df = pd.read_excel("outputs_manual_llama/evaluation_results_['meta.llama3-70b-instruct-v1:0'].xlsx")
+claude_df = pd.read_excel("outputs_claude/evaluation_results_['anthropic.claude-3-5-sonnet-20240620-v1:0'].xlsx")
+gpt_df = pd.read_excel("outputs_gpt4o/evaluation_results_['gpt-4o'].xlsx")
+llama_df = pd.read_excel("outputs_llama/evaluation_results_['meta.llama3-70b-instruct-v1:0'].xlsx")
 
 metrics = [
     'bleu', 'rouge1', 'rouge2', 'rougeL',
@@ -26,7 +29,7 @@ def extract_method(filename):
         return 'unknown'
 
 
-def bar_plots(summary, llm_name):
+def bar_plots(summary, llm_name, data):
     fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(20, 10))
     axes = axes.flatten()
 
@@ -35,11 +38,11 @@ def bar_plots(summary, llm_name):
         axes[i].set_title(metric)
         axes[i].set_ylabel("Average Score")
         axes[i].set_xlabel("Method")
-    fig.suptitle(f'{llm_name} Evaluation (Data Prepared Manually)')
+    fig.suptitle(f'{llm_name} Evaluation ({data})')
     plt.tight_layout()
     plt.show()
 
-
+# Bar plots for average evaluation metrics by method(base vs tuned vs synthesis) for manually prepared data
 manual_claude_df['method'] = manual_claude_df['output_file'].apply(extract_method)
 claude_summary = manual_claude_df.groupby('method')[metrics].mean().reset_index()
 
@@ -49,9 +52,9 @@ gpt_summary = manual_gpt_df.groupby('method')[metrics].mean().reset_index()
 manual_llama_df['method'] = manual_llama_df['output_file'].apply(extract_method)
 llama_summary = manual_llama_df.groupby('method')[metrics].mean().reset_index()
 
-bar_plots(claude_summary, llm_name="Claude")
-bar_plots(gpt_summary, llm_name="GPT-4o")
-bar_plots(llama_summary, llm_name="Llama")
+# bar_plots(claude_summary, llm_name="Claude", data='Data Prepared Manually')
+# bar_plots(gpt_summary, llm_name="GPT-4o", data='Data Prepared Manually')
+# bar_plots(llama_summary, llm_name="Llama", data='Data Prepared Manually')
 
 # -------------------------------------- XXXX ---------------------------------------
 
@@ -94,7 +97,7 @@ base_summary = base_df.groupby('model')[metrics].mean().reset_index()
 tuned_summary = tuned_df.groupby('model')[metrics].mean().reset_index()
 
 final_table = reformat_summary_table(base_summary, tuned_summary, synthesis_summary)
-# final_table.to_csv('../eval/Final_eval_DMP.csv')
+# final_table.to_csv('../eval/Final_eval_DPM.csv')
 
 # -------------------------------------- XXXX ---------------------------------------
 
@@ -111,15 +114,31 @@ line_data = pd.concat([
 line_df = line_data.melt(id_vars=['model', 'method'], var_name='metric', value_name='score')
 
 # Plot
-plt.figure(figsize=(12, 6))
-for model in line_df['model'].unique():
-    subset = line_df[(line_df['model'] == model) & (line_df['metric'] == 'overall_accuracy')]
-    plt.plot(subset['method'], subset['score'], label=model, marker='o')
+# plt.figure(figsize=(12, 6))
+# for model in line_df['model'].unique():
+#     subset = line_df[(line_df['model'] == model) & (line_df['metric'] == 'overall_accuracy')]
+#     plt.plot(subset['method'], subset['score'], label=model, marker='o')
+#
+# plt.title('Overall Accuracy Comparison Across Methods')
+# plt.xlabel('Method')
+# plt.ylabel('Average Overall Accuracy')
+# plt.legend(title='Model')
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
 
-plt.title('Overall Accuracy Comparison Across Methods')
-plt.xlabel('Method')
-plt.ylabel('Average Overall Accuracy')
-plt.legend(title='Model')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+# -------------------------------------- XXXX ---------------------------------------
+
+# Bar plots for average evaluation metrics by method(base vs tuned vs synthesis) for LLM prepared data
+claude_df['method'] = claude_df['output_file'].apply(extract_method)
+claude_summary = claude_df.groupby('method')[metrics].mean().reset_index()
+
+gpt_df['method'] = gpt_df['output_file'].apply(extract_method)
+gpt_summary = gpt_df.groupby('method')[metrics].mean().reset_index()
+
+llama_df['method'] = llama_df['output_file'].apply(extract_method)
+llama_summary = llama_df.groupby('method')[metrics].mean().reset_index()
+
+# bar_plots(claude_summary, llm_name="Claude", data='Data Prepared by Claude')
+# bar_plots(gpt_summary, llm_name="GPT-4o", data='Data Prepared by GPT')
+# bar_plots(llama_summary, llm_name="Llama", data='Data Prepared Llama')
